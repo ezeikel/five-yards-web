4aa35a838e85ad4b594a2ce253a40d787ef9bace
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-09-10 01:39:59 +0100

    Added Apollo client to the project.

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import gql from 'graphql-tag';
 import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
+const client = new ApolloClient({
+  uri: 'https://w5xlvm3vzz.lp.gql.zone/graphql'
+});
+
+client.query({
+  query: gql`
+    {
+      rates(currency: "USD") {
+        currency
+      }
+    }
+  `
+})
+.then(result => console.log(result));
+
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApolloProvider>
 );
 
 render(app, document.querySelector('#root'));
