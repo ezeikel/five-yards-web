dc0736fb22779f8369578e54a8edf86b93292eb5
Ezeikel Pemberton <ezeikel_pemberton@discovery.com>
2018-09-13 17:11:04 +0100

    Setting credentials to 'include' has broken everything.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import ApolloClient from 'apollo-boost';
+import { ApolloClient, InMemoryCache } from 'apollo-boost';
+import { createHttpLink } from "apollo-link-http";
 import { ApolloProvider } from 'react-apollo';
 import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-const client = new ApolloClient({
+const link = createHttpLink({
   uri: process.env.NODE_ENV === 'production' ? 'https://five-yards-api.herokuapp.com/graphql' : 'http://localhost:7777/graphql',
+  credentials: 'include'
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link,
   onError: (e) => { console.log(e.graphQLErrors) }
 });
 
