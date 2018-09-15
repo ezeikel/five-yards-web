a3f4e341b48cd402966e0a8cda17a9890e2ab2fd
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-09-15 17:37:30 +0100

    Removed apollo-cache-inmemory and apollo-link-http ~ already included with Apollo Boost. Started using built in apollo-link-state

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { ApolloClient } from 'apollo-boost';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { createHttpLink } from "apollo-link-http";
+import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import App from './containers/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-const link = createHttpLink({
-  uri: process.env.NODE_ENV === 'production' ? 'https://five-yards-api.herokuapp.com/graphql' : 'http://localhost:7777/graphql',
-  credentials: 'include'
-});
+const defaultState = {
+  isEditMode: false
+};
 
 const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link
+  uri: process.env.NODE_ENV === 'production' ? 'https://five-yards-api.herokuapp.com/graphql' : 'http://localhost:7777/graphql',
+  credentials: 'include',
+  clientState: {
+    defaults: defaultState,
+    resolvers: {
+      Query: {},
+      Mutation: {}
+    }
+  }
 });
 
 const app = (
