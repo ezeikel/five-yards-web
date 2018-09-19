6b5efbb42799ecb6a8a5c1eae6d9f8e588197a5d
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-09-19 01:33:23 +0100

    Added apollo-cache-persist to project.

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,6 +1,15 @@
 import ApolloClient from 'apollo-boost';
-
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { persistCache } from 'apollo-cache-persist';
 import { defaults, resolvers } from "./store";
+
+const cache = new InMemoryCache();
+
+persistCache({
+  storage: window.localStorage,
+  debug: process.env.NODE_ENV !== 'production',
+  cache
+});
 
 const client = new ApolloClient({
   uri: process.env.NODE_ENV === 'production' ? 'https://five-yards-api.herokuapp.com/graphql' : 'http://localhost:7777/graphql',
@@ -8,7 +17,8 @@ const client = new ApolloClient({
   clientState: {
     defaults,
     resolvers
-  }
+  },
+  cache
 });
 
 client.onResetStore(client.writeDefaults);
