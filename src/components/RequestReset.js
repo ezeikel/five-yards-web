82ffb1bd88a87a03e6f84a0eb2a38889f1506ae6
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-09-30 21:58:57 +0100

    Password reset form styling.

--- src/components/RequestReset.js
+++ src/components/RequestReset.js
@@ -1,53 +1,69 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik } from 'formik';
 import * as Yup from 'yup'
 import { REQUEST_RESET_MUTATION } from '../apollo/queries'
+import Spinner from './Spinner';
+import Button from '../styles/Button';
+import Form from '../styles/Form';
+import FormFields from '../styles/FormFields';
+import FormActions from '../styles/FormActions';
+import FieldSet from '../styles/FieldSet';
+import FormInput from '../styles/FormInput';
+import FormInputError from '../styles/FormInputError';
+import styled from 'styled-components';
 
 const ReqeuestResetSchema = Yup.object().shape({
-  fullName: Yup.string()
-    .min(2, 'Too short!')
-    .max(50, 'Too long')
-    .required('Required'),
-  username: Yup.string()
-    .min(2, 'Too short!')
-    .max(50, 'Too long')
-    .required(),
   email: Yup.string()
     .email('Invalid email')
-    .required('Required'),
-  password: Yup.string()
-    .min(9, 'Too short')
     .required('Required')
 });
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-row-gap: var(--spacing-medium);
+  h1 {
+    margin: 0;
+    font-size: 22px;
+  }
+`;
 
 class RequestReset extends Component {
   render() {
     return (
-      <Mutation mutation={REQUEST_RESET_MUTATION} variables={this.state}>
-        {(reset, { error, loading, called }) => (
-          <Formik
-            initialValues={{ email: '' }}
-            validationSchema={ReqeuestResetSchema}
-            onSubmit={reset}
-          >
-            <Form>
-              <fieldset disabled={loading} aria-busy={loading}>
-                <h2>Request a password reset</h2>
-                {!error && !loading && called && <p>Success! Check your email for a reset link!</p>}
-                <label htmlFor="email">
-                  Email
-                  <Field type="email" name="email" placeholder="kanye@yeezy.com" />
-                  <ErrorMessage name="email" />
-                </label>
-                <button type="submit">Request Reset!</button>
-              </fieldset>
-            </Form>
-          </Formik>
-        )}
-      </Mutation>
-    );
-  }
-}
+      <Wrapper>
+        <h1>Forgot your password?</h1>
+        <Mutation mutation={REQUEST_RESET_MUTATION} variables={this.state}>
+          {(reset, { error, loading, called }) => (
+            <Formik
+              initialValues={{ email: '' }}
+              validationSchema={ReqeuestResetSchema}
+              onSubmit={reset}
+            >
+              {({
+                isSubmitting,
+                errors,
+                touched
+              }) => (
+                <Form>
+                  <FormFields>
+                    {/* {!error && !loading && called && <p>Success! Check your email for a reset link!</p>} */}
+                    <FieldSet>
+                      <label htmlFor="email">Email</label>
+                      {errors.email && touched.email && <FormInputError>{errors.email}</FormInputError>}
+                      <FormInput type="email" name="email" placeholder="kanye@yeezy.com" />
+                    </FieldSet>
+                  </FormFields>
+                  <Button type="submit" disabled={true}>
+                    <span>Reset password</span> {isSubmitting ? <Spinner /> : null}
+                  </Button>
+                </Form>
+              )}
+            </Formik>
+          )}
+        </Mutation>
+      </Wrapper>
+    )
+}}
 
 export default RequestReset;
