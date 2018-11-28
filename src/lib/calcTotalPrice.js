928770f227558072a642e17510aff6db7ea35798
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-11-28 02:30:57 +0000

    Temporary fix for cart breaking the whole app after signin.

--- src/lib/calcTotalPrice.js
+++ src/lib/calcTotalPrice.js
@@ -1,7 +1,6 @@
 export default function calcTotalPrice(cart) {
-  debugger;
   return cart.reduce((tally, cartItem) => {
-    debugger;
+
     if (!cartItem.item) return tally;
     return tally + cartItem.quantity * cartItem.item.price;
   }, 0);
