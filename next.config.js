a9406f05ca58181c8ebc6bc870725b6feb6c46f5
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2020-02-27 15:13:55 +0000

    Simplified next config as per github issue update.

--- next.config.js
+++ next.config.js
@@ -1,37 +1,7 @@
 // next.config.js
 
-// https://github.com/zeit/next.js/issues/5750
-const { PHASE_PRODUCTION_SERVER } =
-  process.env.NODE_ENV === "development"
-    ? {} // We're never in "production server" phase when in development mode
-    : !process.env.NOW_REGION
-    ? require("next/constants") // Get values from `next` package when building locally
-    : require("next-server/constants"); // Get values from `next-server` package when building on now v2
+const withCSS = require("@zeit/next-css");
 
-// TODO: Remember why you left defaultConfig in when not being used
-// eslint-disable-next-line
-module.exports = (phase, { defaultConfig }) => {
-  if (phase === PHASE_PRODUCTION_SERVER) {
-    // Config used to run in production.
-    return {};
-  }
-
-  const withCSS = require("@zeit/next-css");
-
-  return withCSS({
-    target: "serverless",
-    webpack: function(config) {
-      config.module.rules.push({
-        test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-        use: {
-          loader: "url-loader",
-          options: {
-            limit: 100000,
-            name: "[name].[ext]",
-          },
-        },
-      });
-      return config;
-    },
-  });
-};
+module.exports = withCSS({
+  target: "serverless",
+});
