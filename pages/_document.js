20853191c075e6d61771855068e86b8ee03eafe1
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2020-11-03 14:41:00 +0000

    Correct hotjar tracking code.

--- pages/_document.js
+++ pages/_document.js
@@ -47,12 +47,14 @@ class MyDocument extends Document {
       __html: `
         (function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:1965853,hjsv:6};
+            h._hjSettings={hjid:2075970,hjsv:6};
             a=o.getElementsByTagName('head')[0];
             r=o.createElement('script');r.async=1;
             r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
             a.appendChild(r);
         })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+
+        <!-- Hotjar Tracking Code for https://fiveyards.app -->
       `,
     };
   }
