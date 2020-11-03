f5c8c7c6b72939cab37a05115d3deaebd3bd0cf2
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2020-11-03 13:25:45 +0000

    Added Hotjar script to _document.

--- pages/_document.js
+++ pages/_document.js
@@ -42,6 +42,21 @@ class MyDocument extends Document {
     };
   }
 
+  setHotjarTag() {
+    return {
+      __html: `
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:1965853,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      `,
+    };
+  }
+
   render() {
     return (
       <html>
@@ -53,6 +68,7 @@ class MyDocument extends Document {
             src="https://www.googletagmanager.com/gtag/js?id=G-PKC9SQ34MW"
           ></script>
           <script dangerouslySetInnerHTML={this.setGoogleAnalyticsTags()} />
+          <script dangerouslySetInnerHTML={this.setHotjarTag()} />
           <script dangerouslySetInnerHTML={this.setCypressReactDevTools()} />
         </Head>
         <body>
